c2091494a90357e373451d18ba5b4c5e638735c4
yanpolak <106885425+yanpolak@users.noreply.github.com>
2022-12-19 19:05:07 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymarival <ymarival@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 18:00:00 by cvidon            #+#    #+#             */
+/*   Updated: 2022/12/18 19:12:42 by ymarival         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_next(char **temp)
+{
+	char	*line;
+	char	*ptr;
+
+	ptr = *temp;
+	while (*ptr && *ptr != '\n')
+		++ptr;
+	ptr += (*ptr == '\n');
+	line = ft_substr (*temp, 0, (size_t)(ptr - *temp));
+	if (!line)
+	{
+		free (*temp);
+		return (NULL);
+	}
+	ptr = ft_substr (ptr, 0, ft_strlen (ptr));
+	free (*temp);
+	*temp = ptr;
+	return (line);
+}
+
+static char	*ft_read(char *temp, int fd, char *buf)
+{
+	ssize_t		r;
+
+	r = 1;
+	while (r && !ft_strchr (temp, '\n'))
+	{
+		r = read (fd, buf, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free (buf);
+			free (temp);
+			return (NULL);
+		}
+		buf[r] = 0;
+		temp = ft_strjoin_free_s1 (temp, buf);
+		if (!temp)
+		{
+			free (buf);
+			return (NULL);
+		}
+	}
+	free (buf);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*temp[OPEN_MAX];
+	char		*buf;
+
+	if (fd == -1 || BUFFER_SIZE < 1)
+		return (NULL);
+	if (!temp[fd])
+		temp[fd] = ft_strdup("");
+	if (!temp[fd])
+		return (NULL);
+	buf = malloc (sizeof (*buf) * (BUFFER_SIZE + 1));
+	if (!buf)
+	{
+		free (temp[fd]);
+		return (NULL);
+	}
+	temp[fd] = ft_read (temp[fd], fd, buf);
+	if (!temp[fd])
+		return (NULL);
+	if (!*temp[fd])
+	{
+		free (temp[fd]);
+		temp[fd] = NULL;
+		return (NULL);
+	}
+	return (ft_next(&temp[fd]));
+}
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymarival <ymarival@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 18:15:19 by ymarival          #+#    #+#             */
+/*   Updated: 2022/12/19 18:16:15 by ymarival         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+char	*ft_strchr(char const *s, int c);
+size_t	ft_strlen(char const *str);
+char	*ft_strdup(char const *s1);
+char	*ft_strjoin_free_s1(char *s1, char const *s2);
+char	*ft_substr(char const *str, unsigned int start, size_t size);
+char	*get_next_line(int fd);
+
+#endif
